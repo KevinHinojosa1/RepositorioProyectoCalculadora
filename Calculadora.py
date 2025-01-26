c825947f56bca5fcbfcdea57a93b2e24d83f82d1
“KevinHinojosa1” <steven_hinojosa@hotmail.com>
2025-01-25 22:44:07 -0500

    Cambio calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,18 @@
-#definicion de la suma 
-def suma(a,b):
-    return a+b
+# Hinojosa Viteri Kevin Steven 
 
+def suma(a, b):
+    """Suma dos números"""
+    return a + b
+
+def resta(a, b):
+    """Resta dos números"""
+    return a - b
+
+def multiplica(a, b):
+    """Multiplica dos números"""
+    return a * b
+
+# Ejemplo de uso
+print(suma(5, 3))        # Imprime 8
+print(resta(10, 4))      # Imprime 6
+print(multiplica(6, 7))  # Imprime 42
